742a091bfc199b3b1b878668fe2bb9ccccaa36e4
Hrishikesh <kulkarni_hrishikesh@yahoo.com>
2022-07-03 12:47:08 +0530

    added parameter service project

---
+++ ParametersService/src/main/java/com/parameters/info/ParametersServiceApplication.java
@@ -0,0 +1,13 @@
+package com.parameters.info;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParametersServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParametersServiceApplication.class, args);
+	}
+
+}
---
+++ ParametersService/src/test/java/com/parameters/info/ParametersServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parameters.info;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParametersServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
