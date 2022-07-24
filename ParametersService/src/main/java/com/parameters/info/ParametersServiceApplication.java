b90fef2c7fc8017b370285e4b9e6757d9e27aaa3
Hrishikesh <kulkarni_hrishikesh@yahoo.com>
2022-07-24 22:04:16 +0530

    parameter service

--- ParametersService/src/main/java/com/parameters/info/ParametersServiceApplication.java
+++ ParametersService/src/main/java/com/parameters/info/ParametersServiceApplication.java
@@ -1,21 +1,15 @@
 package com.parameters.info;
 
-import javax.annotation.PostConstruct;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
-import com.parameters.info.entity.parameter;
-import com.parameters.info.service.Repo;
-
 @SpringBootApplication
 public class ParametersServiceApplication {
-	@Autowired
-	Repo repo;
+//	@Autowired
+//	RepoM267 repo;
 	public static void main(String[] args) {
 		SpringApplication.run(ParametersServiceApplication.class, args);
 
@@ -27,10 +21,10 @@ public class ParametersServiceApplication {
 		return new RestTemplate();
 	}
 
-
-	@PostConstruct
-	public void postconstruct() {
-		repo.save(new parameter(1,"TR-C4-V13VBN-00050",72.5,17.5,70,0));
-	}
+//
+//	@PostConstruct
+//	public void postconstruct() {
+//		repo.save(new M267(1,"TR-C4-V13VBN-00050",72.5,17.5,70,0));
+//	}
 
 }
--- ParametersService/src/main/java/com/parameters/info/controller/PrametersController.java
+++ ParametersService/src/main/java/com/parameters/info/controller/PrametersController.java
@@ -1,27 +1,55 @@
 package com.parameters.info.controller;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.parameters.info.entity.parameter;
-import com.parameters.info.service.Repo;
-
 @RestController
-@RequestMapping("/family")
 public class PrametersController {
+
+	
 	@Autowired
-	private Repo repo;
-
-	@GetMapping("/{id}")
-	public ResponseEntity<parameter> getArticle(@PathVariable int id) {
-		parameter param = repo.findById(id).get();
-		return new ResponseEntity<parameter>(param, HttpStatus.OK);
-
+	private JdbcTemplate service;
+	
+	
+	@RequestMapping("/{name}")
+	public ResponseEntity<HttpStatus>createTable(@PathVariable String name){
+		Optional<List<String>> str = Optional.of(new ArrayList<String>(Arrays.asList("PREIP","DDC","DCON","ALDS")));
+		Optional<List<String>> str1 = Optional.of(new ArrayList<String>());
+		Optional<List<String>> str2 = Optional.of(new ArrayList<String>());
+		StringBuilder sqlBuilder = new StringBuilder();
+		if(str.isPresent())
+		for (String string : str.get()) {
+			sqlBuilder.append(string + " DECIMAL,");
+		}
+		
+		if(str1.isPresent())
+			for (String string : str1.get()) {
+				sqlBuilder.append(string + " INTEGER,");
+			}
+		if(str2.isPresent())
+			for (String string : str2.get()) {
+				sqlBuilder.append(string + " BIT,");
+			}
+		String sql = "CREATE TABLE "+name.toString()+" ( ID INTEGER, STANDARDCODE VARCHAR(255), " +sqlBuilder.toString() +" PRIMARY KEY(ID))"; 
+		service.execute(sql);
+		return new ResponseEntity<HttpStatus>(HttpStatus.OK);
 	}
+	
+	
+	
+	
+	
+	
+	
 
 }
